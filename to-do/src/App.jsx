6156c839b1b5066a4bebeb8315c938882b5387a3
daniellberg <danielbergons@gmail.com>
2023-06-27 21:08:08 -0300

    new project with vite+js

---
+++ to-do/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import Todo from './components/todo';
+import './App.css'
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+//todos.map percorre todos os objetos de todos (acima) para que possam ser acessados pelos métodos próprios utilizando . e o tipo do objeto (todo.text, todo.category...)
+
+  return (
+    <div className="app">
+      <h1>Lista de tarefas</h1>
+        <div className="todo-list">
+          {todos.map((todo) =>(
+            <Todo todo={todo} />
+          ))}
+        </div>
+    </div>
+  );
+}
+
+export default App
